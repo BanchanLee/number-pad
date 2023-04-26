ee5911f1d043ff50ce1c082e3aa5b01192ffe44d
Lee <89811076+HarperGLee@users.noreply.github.com>
2023-04-26 13:17:28 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,66 +1,44 @@
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P11, 0)
-pins.digitalWritePin(DigitalPin.P8, 0)
-pins.digitalWritePin(DigitalPin.P5, 0)
+enum RadioMessage {
+    message1 = 49434
+}
+pins.digitalWritePin(DigitalPin.P2, 1)
+pins.digitalWritePin(DigitalPin.P11, 1)
+pins.digitalWritePin(DigitalPin.P8, 1)
+pins.digitalWritePin(DigitalPin.P5, 1)
 pins.setPull(DigitalPin.P16, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P15, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P14, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P13, PinPullMode.PullDown)
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    while (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        basic.showString("1")
+    control.waitForEvent(pins.digitalReadPin(DigitalPin.P2), 1)
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        basic.showIcon(IconNames.Yes)
+        control.waitForEvent(pins.digitalReadPin(DigitalPin.P8), 1)
+        if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+            basic.showIcon(IconNames.Yes)
+            control.waitForEvent(pins.digitalReadPin(DigitalPin.P8), 1)
+            if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+                basic.showIcon(IconNames.Yes)
+                control.waitForEvent(pins.digitalReadPin(DigitalPin.P8), 1)
+                if (pins.digitalReadPin(DigitalPin.P8) == 1) {
+                    basic.showIcon(IconNames.Yes)
+                    control.waitForEvent(pins.digitalReadPin(DigitalPin.P2), 1)
+                    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+                        basic.showIcon(IconNames.Yes)
+                        servos.P0.setAngle(90)
+                    } else {
+                        basic.showIcon(IconNames.No)
+                    }
+                } else {
+                    basic.showIcon(IconNames.No)
+                }
+            } else {
+                basic.showIcon(IconNames.No)
+            }
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    } else {
+        basic.showIcon(IconNames.No)
     }
-    while (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        basic.showString("4")
-    }
-    while (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        basic.showString("7")
-    }
-    while (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        basic.showString("*")
-    }
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P11, 1)
-    while (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        basic.showString("2")
-    }
-    while (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        basic.showString("5")
-    }
-    while (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        basic.showString("8")
-    }
-    while (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        basic.showString("0")
-    }
-    pins.digitalWritePin(DigitalPin.P11, 0)
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    while (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        basic.showString("3")
-    }
-    while (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        basic.showString("6")
-    }
-    while (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        basic.showString("9")
-    }
-    while (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        basic.showString("#")
-    }
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P5, 1)
-    while (pins.digitalReadPin(DigitalPin.P16) == 1) {
-        basic.showString("A")
-    }
-    while (pins.digitalReadPin(DigitalPin.P15) == 1) {
-        basic.showString("B")
-    }
-    while (pins.digitalReadPin(DigitalPin.P14) == 1) {
-        basic.showString("C")
-    }
-    while (pins.digitalReadPin(DigitalPin.P13) == 1) {
-        basic.showString("D")
-    }
-    pins.digitalWritePin(DigitalPin.P5, 0)
 })
